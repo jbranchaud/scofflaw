5d977aa2ab1b33f4af0f0434c6c54ea55143219c
jbranchaud <jbranchaud@gmail.com>
2015-12-29 14:09:44 -0600

    Update the ItemType spec.

--- spec/models/item_type_spec.rb
+++ spec/models/item_type_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 describe 'ItemType' do
-  describe '.new' do
+  describe '.create' do
     it 'creates a new ItemType instance' do
       garnish = ItemType.create(name: 'Garnish')
       expect(garnish).to be
     end
 
     context 'without a name' do
-      it 'raises an error' do
-        expect { ItemType.create! }.to raise_error ActiveRecord::RecordInvalid
+      it 'raises a RecordInvalid error' do
+        expect {
+          ItemType.create!
+        }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
       end
     end
   end
