aa8d102583b418e50a6dd2f5af70918e7dcc75d9
jbranchaud <jbranchaud@gmail.com>
2016-01-05 08:42:35 -0600

    Move the es7 class properties transform to app.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module MixList
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.react.jsx_transform_options = {
+      optional: ["es7.classProperties"]
+    }
   end
 end
