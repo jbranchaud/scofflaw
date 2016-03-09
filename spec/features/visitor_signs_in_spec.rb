098f2a7c94229b0a1958c83ab730ddc39cf2104a
jbranchaud <jbranchaud@gmail.com>
2016-03-09 08:35:13 -0600

    Visitor cannot sign in with invalid email and password

--- spec/features/visitor_signs_in_spec.rb
+++ spec/features/visitor_signs_in_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 describe 'Visitor signs in' do
-  context 'when a valid email and password are provided' do
-    before do
-      FactoryGirl.create(:user, email: 'lizlemon@nbc.com', password: 'password')
-    end
+  before do
+    FactoryGirl.create(:user, email: 'lizlemon@nbc.com', password: 'cheesyblasters')
+  end
 
+  context 'when a valid email and password are provided' do
     scenario 'the visitor is signed in to their account' do
+      visit root_path
+      click_on 'Log In'
+      expect(page).to have_selector('h2', text: 'Log in')
+
+      fill_in 'Email', with: 'lizlemon@nbc.com'
+      fill_in 'Password', with: 'cheesyblasters'
+      click_on 'Log in'
+
+      expect(page).to have_selector('h1', text: 'Recipes')
+      expect(page).to have_content('Hi, lizlemon@nbc.com')
+    end
+  end
+
+  context 'when an invalid email and password are provided' do
+    scenario 'the visitor sees a Failed To Log In message' do
       visit root_path
       click_on 'Log In'
       expect(page).to have_selector('h2', text: 'Log in')
@@ -15,8 +30,8 @@ describe 'Visitor signs in' do
       fill_in 'Password', with: 'password'
       click_on 'Log in'
 
-      expect(page).to have_selector('h1', text: 'Recipes')
-      expect(page).to have_content('Hi, lizlemon@nbc.com')
+      expect(page).to have_selector('h2', text: 'Log in')
+      expect(page).to have_selector('.alert', text: "Invalid email or password")
     end
   end
 end
