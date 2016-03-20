24b0c4acccac7626a4bc195eca361a44cacca0f5
jbranchaud <jbranchaud@gmail.com>
2016-03-20 07:28:29 -0500

    Add some recipes to dev seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Development Seeds
+if Rails.env.development?
+
+  ## Recipes
+  Recipe.all.destroy_all
+
+  FactoryGirl.create :recipe,
+    name: 'Old Fashioned',
+    description: 'a cocktail made by muddling sugar with bitters, then adding alcohol, such as whiskey or brandy, and a twist of citrus rind'
+
+  FactoryGirl.create :recipe,
+    name: 'Sazerac',
+    description: 'a local New Orleans variation of a Cognac or whiskey cocktail, named for the Sazerac de Forge et Fils brand of Cognac brandy that served as its original main ingredient'
+
+  FactoryGirl.create :recipe,
+    name: 'French 75',
+    description: 'a cocktail made from gin, Champagne, lemon juice, and sugar'
+
+  FactoryGirl.create :recipe,
+    name: 'Moscow Mule',
+    description: 'a mule made with a high-quality vodka, a spicy ginger beer, and lime juice, garnished with a slice or wedge of lime'
+end
