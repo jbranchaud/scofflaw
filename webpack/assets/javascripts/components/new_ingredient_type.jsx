9ef114eab8a7b587030d66bc4d5fee84cab9b6bd
jbranchaud <jbranchaud@gmail.com>
2016-03-28 15:55:13 -0500

    Destructure Component for reference

--- webpack/assets/javascripts/components/new_ingredient_type.jsx
+++ webpack/assets/javascripts/components/new_ingredient_type.jsx
@@ -1,7 +1,7 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import $ from 'jquery';
 
-class NewIngredientType extends React.Component {
+class NewIngredientType extends Component {
   static propTypes = {
     name: PropTypes.string,
   }
