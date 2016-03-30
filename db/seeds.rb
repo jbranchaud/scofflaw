b9848906eecce15397e0996495918e0543961f0f
jbranchaud <jbranchaud@gmail.com>
2016-03-30 07:58:56 -0500

    Add reporting to seeds as they are created

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,27 +12,37 @@ if Rails.env.development?
   ## Users
   User.all.destroy_all
 
-  FactoryGirl.create :user, email: 'lizlemon@nbc.com'
-  FactoryGirl.create :user, email: 'kennethpacell@nbc.com'
-  FactoryGirl.create :user, email: 'tracyjordan@nbc.com'
+  puts 'Creating Users:'
+  user1 = FactoryGirl.create :user, email: 'lizlemon@nbc.com'
+  puts "- Creating #{user1.email}"
+  user2 = FactoryGirl.create :user, email: 'kennethpacell@nbc.com'
+  puts "- Creating #{user2.email}"
+  user3 = FactoryGirl.create :user, email: 'tracyjordan@nbc.com'
+  puts "- Creating #{user3.email}"
+  puts ''
 
 
   ## Recipes
   Recipe.all.destroy_all
 
-  FactoryGirl.create :recipe,
+  puts 'Creating Recipes:'
+  recipe1 = FactoryGirl.create :recipe,
     name: 'Old Fashioned',
     description: 'a cocktail made by muddling sugar with bitters, then adding alcohol, such as whiskey or brandy, and a twist of citrus rind'
+  puts "- Creating #{recipe1.name}"
 
-  FactoryGirl.create :recipe,
+  recipe2 = FactoryGirl.create :recipe,
     name: 'Sazerac',
     description: 'a local New Orleans variation of a Cognac or whiskey cocktail, named for the Sazerac de Forge et Fils brand of Cognac brandy that served as its original main ingredient'
+  puts "- Creating #{recipe2.name}"
 
-  FactoryGirl.create :recipe,
+  recipe3 = FactoryGirl.create :recipe,
     name: 'French 75',
     description: 'a cocktail made from gin, Champagne, lemon juice, and sugar'
+  puts "- Creating #{recipe3.name}"
 
-  FactoryGirl.create :recipe,
+  recipe4 = FactoryGirl.create :recipe,
     name: 'Moscow Mule',
     description: 'a mule made with a high-quality vodka, a spicy ginger beer, and lime juice, garnished with a slice or wedge of lime'
+  puts "- Creating #{recipe4.name}"
 end
