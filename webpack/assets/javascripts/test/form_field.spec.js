b3c036a53f27ccbee082a109d2f7fab7bcd8058f
jbranchaud <jbranchaud@gmail.com>
2016-03-30 21:07:14 -0500

    Add an initial set of tests against FormField

---
+++ webpack/assets/javascripts/test/form_field.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow, mount, render } from 'enzyme';
+import FormField from '../components/form_field';
+
+describe("FormField", function() {
+  it("renders the base component", function() {
+    const wrapper = shallow(<FormField />);
+    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('input[type="text"]').length).toEqual(1);
+    expect(wrapper.find('span').text()).toEqual('');
+  });
+
+  it('renders with label text', function() {
+    const wrapper = shallow(<FormField label="Email" />);
+    expect(wrapper.find('label').text()).toEqual('Email');
+  });
+
+  it('contains a text input when type is text', function() {
+    const wrapper = shallow(<FormField type="text" />);
+    expect(wrapper.find('input[type="text"]').length).toEqual(1);
+  });
+
+  it('contains a textarea when type is textarea', function() {
+    const wrapper = shallow(<FormField type="textarea" />);
+    expect(wrapper.find('textarea').length).toEqual(1);
+  });
+
+  it('renders an error when present', function() {
+    const wrapper = shallow(<FormField error="cannot be blank" />);
+    expect(wrapper.find('span').text()).toEqual(' cannot be blank');
+  });
+});
