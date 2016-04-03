7338062cf3592b04baef725e9ea2101bbe3c3ac0
jbranchaud <jbranchaud@gmail.com>
2016-04-03 11:09:42 -0500

    Take advantage of the root path in app.jsx

--- webpack/assets/javascripts/app.jsx
+++ webpack/assets/javascripts/app.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from './store/configure_store';
+import configureStore from 'store/configure_store';
 
-import NewIngredientType from './components/new_ingredient_type';
-import NewRecipe from './components/recipes/new_recipe';
+import NewIngredientType from 'components/new_ingredient_type';
+import NewRecipe from 'components/recipes/new_recipe';
 
 import '../stylesheets/app.scss';
 
