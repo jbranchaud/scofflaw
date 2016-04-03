8820054d3599f82d367460bd103bbd6b135e996c
jbranchaud <jbranchaud@gmail.com>
2016-04-03 12:13:21 -0500

    Update the expectations for an ingredient
    Rather than just checking if a newly created ingredient exists, check
    the name, description, and ingredient type

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,7 +6,9 @@ describe 'Ingredients' do
 
     it 'creates a new ingredient record' do
       lime_wedge = Ingredient.create!(name: 'Lime Wedge', description: 'An 8th of a lime', ingredient_type_id: garnish.id)
-      expect(lime_wedge).to be
+      expect(lime_wedge.name).to eq 'Lime Wedge'
+      expect(lime_wedge.description).to eq 'An 8th of a lime'
+      expect(lime_wedge.ingredient_type.name).to eq 'garnish'
     end
 
     context 'when the name is missing' do
