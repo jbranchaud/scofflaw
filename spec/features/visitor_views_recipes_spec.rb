01fc46b05cea3cb07a41cd6f37f78e34d4af48d6
jbranchaud <jbranchaud@gmail.com>
2016-04-05 19:42:37 -0500

    Use Recipes page object

--- spec/features/visitor_views_recipes_spec.rb
+++ spec/features/visitor_views_recipes_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe 'Visitor views recipes' do
+  let(:recipes_page) { Pages::Recipes.new }
+
   context 'when there are recipes' do
     scenario 'the names of the recipes are displayed' do
       FactoryGirl.create(:recipe, name: 'Sazerac')
@@ -8,6 +10,7 @@ describe 'Visitor views recipes' do
 
       visit recipes_path
 
+      expect(recipes_page).to be_on_page
       expect(page).to have_content 'Sazerac'
       expect(page).to have_content 'Old Fashioned'
     end
@@ -17,6 +20,7 @@ describe 'Visitor views recipes' do
     scenario 'a No Recipes message is displayed' do
       visit recipes_path
 
+      expect(recipes_page).to be_on_page
       expect(page).to have_content 'There are no recipes yet'
     end
   end
